b40809970e37dd25bddf3f04464bea7b8b4b60d2
chiaaa39 <weien2817@gmail.com>
2026-08-10 16:40:37 +0800

    Update

--- server.js
+++ server.js
@@ -29,12 +29,12 @@ app.get('/api/dashboard', async (req, res) => {
   try {
     const dataDir = path.join(__dirname, 'data');
     
-    const staffList = await readCSV(path.join(dataDir, 'Lorry_condition_Staff_Details.csv'));
-    const mainOrders = await readCSV(path.join(dataDir, 'Lorry_condition_Sheet1.csv'));
+    const staffList = await readCSV(path.join(dataDir, 'Lorry condition-Staff Details.csv'));
+    const mainOrders = await readCSV(path.join(dataDir, 'Lorry condition-Sheet1.csv'));
     
     const arrives = {};
     for (let i = 1; i <= 5; i++) {
-      arrives[`arrive_${i}`] = await readCSV(path.join(dataDir, `Lorry_condition_Arrive_${i}.csv`));
+      arrives[`arrive_${i}`] = await readCSV(path.join(dataDir, `Lorry condition-Arrive_${i}.csv`));
     }
 
     const mergedData = mainOrders.map(order => {
